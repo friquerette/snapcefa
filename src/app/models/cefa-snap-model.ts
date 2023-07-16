0fc71c6db8e8fffb0e3cb354b3622a782c0a1d17
Rick <derruette@yahoo.fr>
2023-07-16 22:51:57 +0200

    angular 2k23: lesson 7: add models (entity) CefaSnap with shortcut constuctor

---
+++ src/app/models/cefa-snap-model.ts
@@ -0,0 +1,9 @@
+export class CefaSnap {
+    
+    constructor (public title: string,
+        public description: string, 
+        public createdDate: Date, 
+        public snaps: number, 
+        public imageUrl: string) { }
+
+}
