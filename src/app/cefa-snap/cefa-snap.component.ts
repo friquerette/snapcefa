e733f9c470c0cd5d32546007f16350e77fb3ca44
Rick <derruette@yahoo.fr>
2023-07-17 08:40:26 +0200

    angular 2k23: lesson 7: use models (entity) Cefa

--- src/app/cefa-snap/cefa-snap.component.ts
+++ src/app/cefa-snap/cefa-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CefaSnap } from '../models/cefa-snap-model';
 
 @Component({
   selector: 'app-cefa-snap',
@@ -6,26 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cefa-snap.component.scss']
 })
 export class CefaSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() cefaSnap!: CefaSnap;
+
   addSnap!: boolean;
   buttonText!: string;
 
   ngOnInit() {
-    this.title = 'The Face';
-    this.description = 'description of one face';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Chiltern_bears_Chesham_Museum.jpg/440px-Chiltern_bears_Chesham_Museum.jpg';
     this.addSnap = true;
     this.buttonText = 'Oh Snap!';
   }
   
   onAddSnap() {
-    this.snaps = this.addSnap ? this.snaps+1 : this.snaps-1;
+    this.cefaSnap.snaps = this.addSnap ? this.cefaSnap.snaps+1 : this.cefaSnap.snaps-1;
     this.buttonText = this.addSnap ? 'Oups Unsnap!' : 'Oh Snap!';
     this.addSnap = !this.addSnap;
   }
