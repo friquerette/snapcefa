83d9c6a54275e87d849740f653398931accf0ff9
Rick <derruette@yahoo.fr>
2023-07-19 23:07:02 +0200

    angular 2k23: lesson 12: pipe date with setting of the french locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { CefaSnapComponent } from './cefa-snap/cefa-snap.component';
@@ -12,7 +14,13 @@ import { CefaSnapComponent } from './cefa-snap/cefa-snap.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'fr_FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default)
+  }
+}
